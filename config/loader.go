1100e33ef691224e0fa86799885e485ff051f652
Zero <john.royer@gmail.com>
2026-05-01 18:05:35 +0800

    define config structure

--- config/loader.go
+++ config/loader.go
@@ -2,6 +2,21 @@ package config
 
 import "os"
 
+type Config struct {
+	Server struct {
+		Address string
+		Port    int
+	}
+
+	Proxy struct {
+		Timeout int
+	}
+
+	Auth struct {
+		Tokens []string
+	}
+}
+
 func fileIsExist() bool {
 	if _, err := os.Stat("./config.toml"); err == nil {
 		return true
