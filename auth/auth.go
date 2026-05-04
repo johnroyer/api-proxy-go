caee6280da7b87fad301a3e4e312a75d1b879122
Zero <john.royer@gmail.com>
2026-05-04 14:45:59 +0800

    return map pointer instead of value

--- auth/auth.go
+++ auth/auth.go
@@ -6,14 +6,14 @@ type Tokens struct {
 	token string
 }
 
-func readFromConfig(config config.Config) (map[string]bool, error) {
+func readFromConfig(config config.Config) (*map[string]bool, error) {
 	size := len(config.Auth.Tokens)
 
 	tokens := make(map[string]bool, size)
 	for _, token := range config.Auth.Tokens {
 		tokens[token] = true
 	}
-	return tokens, nil
+	return &tokens, nil
 }
 
 func IsValid(token string, allowTokens *map[string]bool) bool {
