df56847fd1e4e432cea1f4d0dd6bdded26b382b3
Zero <john.royer@gmail.com>
2026-05-04 17:13:24 +0800

    remove client test
    不要測試有錯誤的測試

--- http-client/client_test.go
+++ http-client/client_test.go
@@ -3,22 +3,5 @@ package http_client
 import "testing"
 
 func TestMakeHttpRequest(t *testing.T) {
-	var request ProxyRequest
-	request = ProxyRequest{
-		Method: "GET",
-		Url:    "https://zeroplex.tw/ip",
-	}
 
-	response, err := SendRequest(request)
-	if err != nil {
-		t.Error("send request failed: " + err.Error())
-	}
-
-	if response.StatusCode != 200 {
-		t.Error("unexpected status code: " + string(rune(response.StatusCode)))
-	}
-
-	if response.Body == nil {
-		t.Error("response body should not be empty")
-	}
 }
