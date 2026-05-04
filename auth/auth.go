08c10533f2be8b18a1ab811f3a16b70a4f9f10e8
Zero <john.royer@gmail.com>
2026-05-04 13:40:50 +0800

    change return type

--- auth/auth.go
+++ auth/auth.go
@@ -6,12 +6,12 @@ type Tokens struct {
 	token string
 }
 
-func readFromConfig(config config.Config) error {
+func readFromConfig(config config.Config) ([]string, error) {
 	size := len(config.Auth.Tokens)
 
 	tokens := make([]string, size)
 	for i := 0; i < size; i++ {
 		tokens[i] = config.Auth.Tokens[i]
 	}
-	return nil
+	return tokens, nil
 }
