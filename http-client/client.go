0a499558de17594d337fd02f2e276023ab9fffd0
Zero <john.royer@gmail.com>
2026-05-04 12:45:28 +0800

    make HTTP request

--- http-client/client.go
+++ http-client/client.go
@@ -10,3 +10,11 @@ type Response struct {
 	StatusCode int
 	Body       []byte
 }
+
+func NewRequest(method, url string, header map[string]string) *Request {
+	return &Request{
+		Method: method,
+		URL:    url,
+		Header: header,
+	}
+}
