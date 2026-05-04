d3bdd15f61d9c186805b362edd442b08bea4319a
Zero <john.royer@gmail.com>
2026-05-04 13:23:15 +0800

    update return type

--- http-client/client.go
+++ http-client/client.go
@@ -15,23 +15,23 @@ type ProxyResponse struct {
 	Body       []byte
 }
 
-func SendRequest(req ProxyRequest) ProxyResponse {
+func SendRequest(req ProxyRequest) (*ProxyResponse, error) {
 	httpReq, err := http.NewRequest(req.Method, req.Url, nil)
 	if err != nil {
-		return ProxyResponse{StatusCode: 500, Body: []byte("Error creating request")}
+		return &ProxyResponse{StatusCode: 500, Body: []byte("Error creating request")}, err
 	}
 
 	client := &http.Client{}
 	response, err := client.Do(httpReq)
 	if err != nil {
-		return ProxyResponse{StatusCode: 500, Body: []byte("Error sending request")}
+		return &ProxyResponse{StatusCode: 500, Body: []byte("Error sending request")}, err
 	}
 
 	statusCode := response.StatusCode
 	htmlBytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		return ProxyResponse{StatusCode: 500, Body: []byte("Error reading response body")}
+		return &ProxyResponse{StatusCode: 500, Body: []byte("Error reading response body")}, err
 	}
 
-	return ProxyResponse{StatusCode: statusCode, Body: htmlBytes}
+	return &ProxyResponse{StatusCode: statusCode, Body: htmlBytes}, nil
 }
