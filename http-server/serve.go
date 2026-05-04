8fed2e6c3524b7c7913f5e3d28e6fe118b7b3dbd
Zero <john.royer@gmail.com>
2026-05-04 14:03:13 +0800

    add user input struct

--- http-server/serve.go
+++ http-server/serve.go
@@ -2,6 +2,9 @@ package http_server
 
 import "net/http"
 
+type UserInput struct {
+	Url string
+}
 func getBearerToken(r *http.Request) string {
 	token := r.Header.Get("Authorization")
 	if token == "" {
