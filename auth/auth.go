71ca9e090c06c39e760ba969e16fd1ac5f715c52
Zero <john.royer@gmail.com>
2026-05-04 13:44:02 +0800

    change []string to map[string]bool
    搜尋速度較快

--- auth/auth.go
+++ auth/auth.go
@@ -6,12 +6,12 @@ type Tokens struct {
 	token string
 }
 
-func readFromConfig(config config.Config) ([]string, error) {
+func readFromConfig(config config.Config) (map[string]bool, error) {
 	size := len(config.Auth.Tokens)
 
-	tokens := make([]string, size)
-	for i := 0; i < size; i++ {
-		tokens[i] = config.Auth.Tokens[i]
+	tokens := make(map[string]bool, size)
+	for _, token := range config.Auth.Tokens {
+		tokens[token] = true
 	}
 	return tokens, nil
 }
