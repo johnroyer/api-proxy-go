b943c25b8cc13adddaab9d912364a146b8048aab
Zero <john.royer@gmail.com>
2026-05-19 19:16:32 +0800

    add log file status chcker

--- logger/file.go
+++ logger/file.go
@@ -6,3 +6,12 @@ func IsExists(filePath string) bool {
 	_, err := os.Stat(filePath)
 	return !os.IsNotExist(err)
 }
+
+func IsWritable(filePath string) bool {
+	file, err := os.OpenFile(filePath, os.O_WRONLY, 0644)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+	return true
+}
